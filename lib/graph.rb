9342f19e33a6f90e2a5a2c226d90c323baf0f7c1
John Jarvis <john@jarv.org>
2017-03-26 12:54:59 +0200

    Update dependencies for packages that already exist.
    Adds a new test and logic to handle this requirement,
    initially missed in the INSTRUCTIONS.md file:
    
    ```
    If a package already exists, then its list of dependencies is updated to
    the one provided with the latest command.
    ```

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,7 +1,13 @@
 class Graph
-  attr_accessor :graph
   def initialize
+    # the package graph is hash map, containing 
+    # a mapping of packages to the list of 
+    # their dependencies
     @graph = {}
+    # the package dep_index is a counter used
+    # to keep track of the total number of
+    # dependencies for every package across
+    # the entire graph for fast lookups.
     @dep_index = Hash.new(0)
   end
 
@@ -10,30 +16,38 @@ class Graph
   end
 
   def index_pkg(pkg, deps = [])
-    return true if has_pkg?(pkg)
     deps.each do |d|
       return false unless has_pkg?(d)
     end
-    deps.each { |d| @dep_index[d] += 1 }
-    graph[pkg] = deps
+    remove_deps_from_index(@graph[pkg]) if has_pkg?(pkg)
+    add_deps_to_index(deps)
+    @graph[pkg] = deps
     true
   end
 
   def remove_pkg(pkg)
     return true unless has_pkg?(pkg)
     return false if dep_exists?(pkg)
-    graph[pkg].each{ |d| @dep_index[d] -= 1 }
-    graph.delete(pkg)
+    remove_deps_from_index(@graph[pkg])
+    @graph.delete(pkg)
     true
   end
 
   private 
+
+  def remove_deps_from_index(deps)
+    deps.each{ |d| @dep_index[d] -= 1 }
+  end
+
+  def add_deps_to_index(deps)
+    deps.each { |d| @dep_index[d] += 1 }
+  end
 
   def dep_exists?(pkg)
     @dep_index.key?(pkg) && (@dep_index[pkg] > 0)
   end
 
   def has_pkg?(pkg)
-    graph.key?(pkg)
+    @graph.key?(pkg)
   end
 end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -23,6 +23,7 @@ describe Graph do
     before(:each) do
       graph.index_pkg("nginx")
       graph.index_pkg("perl")
+      graph.index_pkg("bash")
       graph.index_pkg("apache", ["perl"])
     end
 
@@ -32,13 +33,23 @@ describe Graph do
       expect(graph.query_pkg("nginx")).to be_truthy
     end
 
+    it "should index a package that already exist and update dependencies" do
+      # pkg perl still has dependencies
+      expect(graph.remove_pkg("perl")).to be_falsey
+      # remove the perl dependency by updating apache's deps
+      expect(graph.index_pkg("apache", ["bash"])).to be_truthy
+      # pkg perl can now be removed from the graph
+      expect(graph.remove_pkg("perl")).to be_truthy
+    end
+
+
     it "should index a package that doesn't exist" do
       expect(graph.query_pkg("ruby")).to be_falsey
       expect(graph.index_pkg("ruby")).to be_truthy
       expect(graph.query_pkg("ruby")).to be_truthy
     end
 
-    it "should not index a package that has deps that don't exist" do
+    it "should not index a package that has deps that don't exist in the graph" do
       expect(graph.query_pkg("ruby")).to be_falsey
       expect(graph.index_pkg("ruby", ["pkg1", "pkg2"])).to be_falsey
       expect(graph.query_pkg("ruby")).to be_falsey
